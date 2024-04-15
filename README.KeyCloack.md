d045e470fc738ddb4fa8ffe0bf10aa36bc4b9a46
vadim <vad.pr.it@gmail.com>
2024-04-15 17:03:13 +0300

    Add FastAPIKeycloak (2)

--- README.KeyCloack.md
+++ README.KeyCloack.md
@@ -6,3 +6,6 @@ https://pypi.org/project/python-keycloak/
 # Keycloak sample configuration
 https://fastapi-keycloak.code-specialist.com/keycloak_configuration/
 
+
+Add all roles to service-account-admin-cli
+Add to admin-cli-dedicated "Full scope allowed"
--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -10,10 +10,10 @@ metadata.create_all(engine)
 app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs")
 
 idp = FastAPIKeycloak(
-    server_url="http://keycloak.loc/auth",
-    # server_url="http://keycloak.loc/realms/FastAPIRealm/protocol/openid-connect/auth",
+    server_url="http://172.17.0.1:7070",
     client_id="FastAPIClient",
     client_secret="OASGV2HuGc1YcRFMJbqvr8vfutthmIdL",
+    admin_client_id="admin-cli",
     admin_client_secret="PO8uim2tII5DKU2eQu3Yey08k1HTlZ3G",
     realm="FastAPIRealm",
     callback_uri="http://movie.ng/callback"
@@ -21,13 +21,29 @@ idp = FastAPIKeycloak(
 idp.add_swagger_config(app)
 
 @app.get("/admin")
-def admin(user: OIDCUser = Depends(idp.get_current_user(required_roles=["admin"]))):
-    return f'Hi premium user {user}'
-
+# def admin(user: OIDCUser = Depends(idp.get_current_user(required_roles=["admin"]))):
+def admin(user: OIDCUser = Depends(idp.get_current_user)):
+    u = user()
+    return f'Hi premium user {u}'
 
 @app.get("/user/roles")
 def user_roles(user: OIDCUser = Depends(idp.get_current_user)):
     return f'{user.roles}'
+
+@app.get("/login-link", tags=["auth-flow"])
+def login_redirect():
+    return idp.login_uri
+
+
+@app.get("/callback", tags=["auth-flow"])
+def callback(session_state: str, code: str):
+    return idp.exchange_authorization_code(session_state=session_state, code=code)
+
+
+@app.get("/logout", tags=["auth-flow"])
+def logout():
+    return idp.logout_uri
+
 
 @app.on_event("startup")
 async def startup():
